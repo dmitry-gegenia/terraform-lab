961458f21ddf35e06f11f1bb2a464dcb6d91c727
Dmitry Gegenia <dmitry@gegenia.dev>
2022-03-27 16:26:39 +0300

    added nginx load balancer

---
+++ lb.tf
@@ -0,0 +1,46 @@
+resource "aws_lb" "lab-nginx-lb" {
+  name               = "lan-nginx"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.public-sg.id]
+  subnets            = [aws_subnet.lab-public-subnet-1.id, aws_subnet.lab-public-subnet-2.id]
+
+  tags = {
+    Environment = "Lab"
+  }
+}
+
+resource "aws_lb_target_group" "lab-nginx-lb-tg" {
+  name        = "Lab-nginx-target-group"
+  port        = 80
+  target_type = "instance"
+  vpc_id      = aws_vpc.lab.id
+  protocol    = "HTTP"
+  health_check {
+    enabled  = true
+    interval = 10
+    port     = 80
+    protocol = "HTTP"
+    path     = "/"
+    matcher  = "200-299"
+  }
+  tags = {
+    Name = "lab-frontend-group"
+  }
+}
+
+resource "aws_lb_listener" "lab-nginx-lb-l" {
+  load_balancer_arn = aws_lb.lab-nginx-lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lab-nginx-lb-tg.arn
+  }
+}
+
+resource "aws_autoscaling_attachment" "nginx-asg-attachment" {
+  autoscaling_group_name = aws_autoscaling_group.nginx-asg.id
+  lb_target_group_arn    = aws_lb_target_group.lab-nginx-lb-tg.arn
+}
